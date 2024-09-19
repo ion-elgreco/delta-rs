d41024b92e263a8088500b7e72a2d0b3f8d1ba34
R. Tyler Croy <rtyler@brokenco.de>
2024-09-19 13:38:11 +0000

    chore: clean up some unused imports

--- crates/core/src/delta_datafusion/mod.rs
+++ crates/core/src/delta_datafusion/mod.rs
@@ -78,7 +78,7 @@ use crate::delta_datafusion::expr::parse_predicate_expression;
 use crate::delta_datafusion::schema_adapter::DeltaSchemaAdapterFactory;
 use crate::errors::{DeltaResult, DeltaTableError};
 use crate::kernel::{
-    Add, DataCheck, EagerSnapshot, Invariant, LogicalFile, Snapshot, StructTypeExt,
+    Add, DataCheck, EagerSnapshot, Invariant, Snapshot, StructTypeExt,
 };
 use crate::logstore::LogStoreRef;
 use crate::table::builder::ensure_table_uri;
